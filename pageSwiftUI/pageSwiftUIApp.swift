4f1f4195265926ecff88192330b0dd85fb0ef8a8
Robert0321 <shat0806449@gmail.com>
2022-11-24 11:39:20 +0800

    Initial Commit

---
+++ pageSwiftUI/pageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pageSwiftUIApp.swift
+//  pageSwiftUI
+//
+//  Created by robert on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct pageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
